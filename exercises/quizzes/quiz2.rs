bd2f306f555a093fd974aff9dc7f70797d9ce01f
Gustavo Bittencourt Satheler <gustavosatheler@gmail.com>
2026-02-06 19:18:58 -0300

    feat: finish quizz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -26,8 +26,16 @@ enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input_vec: Vec<(String, Command)>) -> Vec<String> {
+        input_vec
+            .into_iter()
+            .map(|(text, command)| match command {
+                Command::Uppercase => text.to_uppercase(),
+                Command::Trim => text.trim().to_string(),
+                Command::Append(s) => text + &"bar".repeat(s),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -39,6 +47,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
